f3c29430e15e1a58e2facdc6421bb41b6197ca9a
oleno <pirit.lena@gmail.com>
2016-11-06 23:46:00 +0200

    testing

--- src/main/java/com/brainacad/studyproject/service/Application.java
+++ src/main/java/com/brainacad/studyproject/service/Application.java
@@ -3,6 +3,7 @@ package com.brainacad.studyproject.service;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello Application !");
+        //System.out.println("Hello Application!");  // failed test; expected: Hello! actual: Hello Application!
+        System.out.println("Hello!");
     }
 }
--- src/test/java/com/brainacad/studyproject/ApplicationTest.java
+++ src/test/java/com/brainacad/studyproject/ApplicationTest.java
@@ -1,4 +1,51 @@
 package com.brainacad.studyproject;
 
+import com.brainacad.studyproject.service.Application;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class ApplicationTest {
+
+    private File file;
+
+    @Before
+    public void setUp() {
+        file = new File("hello.txt");
+    }
+
+    @After
+    public void tearDown() {
+        file.delete();
+    }
+
+    @Test
+    public void testHello() {
+
+        System.out.println("Test"); // stub
+
+        try (
+             FileOutputStream fos = new FileOutputStream(file);
+             PrintStream out = new PrintStream(fos);
+             FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)
+        ) {
+            // arrange
+            System.setOut(out);
+
+            // act
+            Application.main(null);
+
+            //assert
+            assertEquals("Unexpected output", "Hello!", br.readLine());
+
+        } catch (IOException e) {
+            fail("IO Problems");
+        }
+    }
 }
