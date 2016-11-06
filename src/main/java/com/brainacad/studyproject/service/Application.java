4ebd298d168f7bf38d4c97ad08447d9b271debd6
oleno <pirit.lena@gmail.com>
2016-11-06 23:39:02 +0200

    Initial

---
+++ src/main/java/com/brainacad/studyproject/service/Application.java
@@ -0,0 +1,8 @@
+package com.brainacad.studyproject.service;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Application !");
+    }
+}
